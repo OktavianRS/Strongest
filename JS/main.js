6ed4a7b859f50783709b8d4e8fb75758fc0c7ab1
OktavianRS <edarli@mail.ru>
2014-11-29 13:27:06 +0200

    in working progress

---
+++ JS/main.js
@@ -0,0 +1,5 @@
+jQuery(function($){
+     $(".stats > .container > .ba-grid > .ba-width-1-2:first-child >div> div").click(function(){
+        $(".stats > .container > .ba-grid > .ba-width-1-2:first-child >div>p").toggleClass("open");
+    });
+});
